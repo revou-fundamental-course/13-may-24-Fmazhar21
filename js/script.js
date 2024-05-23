2e77a79027daa6b78ba17c005e83c83b00189efb
Fikri <fikrimuhaimiazhar21@gmail.com>
2024-05-23 16:41:38 +0700

    Final script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,27 @@
 // Ini JS
+//let isKelilingVisible = false; //keliliang is hide
+//function hideor show() {
+   // if (isKelilingVisible == false) {
+   //     isKelilingVisible == true;
+   //     document.getElementById('luas-section').style.display = 'none';
+   //     document.getElementById('keliling-section').style.display = 'block';
+   // } else {
+       // isKelilingVisible == false;
+       // document.getElementById('luas-section').style.dispnlay = 'block';
+       // document.getElementById('keliling-section').style.dispnlay = 'hide';
+  //  }
+//}
+
+function validation ( {
+       let alasValue = document.getElementById('alas-input').value;
+       let tinggiValue = document.getElementById('tinggi-input').value;
+    console.log(alasValue)
+    console.log(tinggiValue) 
+
+    if (alasValue != '' && tinggiValue != '') {
+        console.log('sukses validasi');
+    }else{
+        alert {'inputan kosong'};
+    }
+
+)
